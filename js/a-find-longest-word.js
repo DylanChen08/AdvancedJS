45ffdd2a24b931a5c37a017b4570461e88dee075
DC <3532753799@qq.com>
2026-06-16 23:44:01 +0800

    refactor: improve longest word finding logic with clearer variable names and streamlined loop structure

--- js/a-find-longest-word.js
+++ js/a-find-longest-word.js
@@ -1,21 +1,60 @@
-var findLongestWord = function(s, dictionary) {
-    let ans = "";
+// var findLongestWord = function(s, dictionary) {
+//     let ans = "";
   
+//     for (const word of dictionary) {
+//         // j => word的指针
+//       let j = 0;
+//         // i => s的指针
+//       for (let i = 0; i < s.length; i++) {
+//         if (s[i] === word[j]) j++;
+//       }
+  
+//       if (j === word.length &&
+//           (word.length > ans.length ||
+//           (word.length === ans.length && word < ans))) {
+//         ans = word;
+//       }
+//     }
+  
+//     return ans;
+//   };
+
+
+// 更好
+var findLongestWord = function (s, dictionary) {
+    let result = ""
+
     for (const word of dictionary) {
-        // j => word的指针
-      let j = 0;
-        // i => s的指针
-      for (let i = 0; i < s.length; i++) {
-        if (s[i] === word[j]) j++;
-      }
-  
-      if (j === word.length &&
-          (word.length > ans.length ||
-          // 情况2 - word和ans长度相同，字典序更小 word < ans
-          (word.length === ans.length && word < ans))) {
-        ans = word;
-      }
+
+        // j 指向 word
+        let j = 0
+
+        // i 指向 s
+        let i = 0
+
+        while (j < word.length && i < s.length) {
+
+            if (word[j] === s[i]) {
+                j++
+            }
+
+            // i 始终向前走
+            i++
+        }
+
+        if (
+            j === word.length &&
+            (
+                word.length > result.length ||
+                (
+                    word.length === result.length &&
+                    word < result
+                )
+            )
+        ) {
+            result = word
+        }
     }
-  
-    return ans;
-  };
+
+    return result
+}
